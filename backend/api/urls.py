e5c33c28b2d66299c5f5c591046837885e74b966
Tae Hee Choi <dadac76@hotmail.com>
2022-10-26 19:04:53 -0700

    .

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,5 @@ from .views import FileView, WeightPrediction
 
 urlpatterns = [
     path('weight/', WeightPrediction.as_view(), name='weight_prediction'),
-    path('ocr-file/', FileView.as_view(), name='ocr-file')
+    path('ocr-file/', FileView.as_view(), name='ocr-file'),
 ]
